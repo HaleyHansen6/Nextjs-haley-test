3ea71d8445869a284babd1881528abefb98b92a0
HHansen6 <HaleyHansen6534@gmail.com>
2024-03-23 15:27:51 -0700

    changes

---
+++ index.test.js
@@ -0,0 +1,9 @@
+// my-test-file.test.js
+
+// This is an empty Jest test file
+// You can write your test cases in this file
+
+// Example of a test case:
+test('Example test', () => {
+  // Your test code goes here
+});
