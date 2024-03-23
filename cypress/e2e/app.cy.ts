57c92abac10047b1159b733a37f6e82073460a6b
HHansen6 <HaleyHansen6534@gmail.com>
2024-03-23 15:43:25 -0700

    changes

---
+++ cypress/e2e/app.cy.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+
+// Empty Cypress E2E Test
+describe('Empty Test', () => {
+  it('should pass without any assertions', () => {
+    // This is an empty test.
+    // You can add your test code here.
+    // For now, it does nothing and will pass.
+  })
+})
+
+export {}
